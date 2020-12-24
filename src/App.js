c19d04e3dc621d049986d9336e7dcf6e7e4f16c6
Chaitanya <chaitu.vsh@gmail.com>
2020-12-24 23:52:52 +0530

    Added BookList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import "./App.css";
+
+
+//Components
+import BookList from './components/BookList';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="App">
+        <h1>Chaitu's Reading list</h1>
+        <BookList/>
       </div>
     )
   }
---
+++ src/components/BookList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BookList = () => {
+    return(
+        <div className="bookList">
+            <h2>Book 1</h2>
+            <h2>Book 2</h2>
+        </div>
+    );
+}
+
+export default BookList;
